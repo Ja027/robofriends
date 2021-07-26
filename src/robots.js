6b3c7c945ca9d5914a722b1c0699aedd1abddf60
Joshua Artavia <Joshuaartavia22@gmail.com>
2021-07-26 09:02:18 -0500

    uploading learning project

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: 'Bret',
+        email: 'sincere@april.biz'
+    },
+    {
+        id: 2,
+        name: 'John Stewart',
+        username: 'Johnny',
+        email: 'johnS@stewart.tv'
+    },
+    {
+        id: 3,
+        name: 'Cassandra Ruiz',
+        username: 'Cassy',
+        email: 'Cassidy@ruiz.net'
+    },
+    {
+        id: 4,
+        name: 'Lionel Richie',
+        username: 'Richie',
+        email: 'Lionel@Richie.com'
+    },
+    {
+        id: 5,
+        name: 'Some Name',
+        username: 'Some',
+        email: 'some@name.org'
+    }
+]
